12187c83217486e5271a26c0a27d25c9b7e9374c
George <gio.digmel@gmail.com>
2023-03-03 21:28:24 -0600

    chore: added logo to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { RocketIcon } from "../icons";
+import { Logo } from "../icons";
 
 export const Header = () => {
   return (
     <div className="pt-4 pb-2 px-6 md:px-24 flex justify-between  border-zinc-800 border-b border-opacity-10 ">
-      <RocketIcon />
+      <Logo />
     </div>
   );
 };
--- icons/index.ts
+++ icons/index.ts
@@ -1,1 +1,2 @@
 export * from "./RocketIcon";
+export * from "./Logo";
