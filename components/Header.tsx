ab068e58c41af5990d09fa033a007df21ac0b0aa
George <gio.digmel@gmail.com>
2023-03-04 19:10:26 -0600

    chore: add reload when press Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,30 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { Logo, MenuIcon } from "../icons";
 
 export const Header = () => {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push("/");
+  };
+
+  const openMenu = () => {
+    // TODO: Add open menu logic here
+  };
+
   return (
     <div className="py-2 md:px-24 flex justify-between items-center border-primary border-b border-opacity-10">
-      <div className="md:scale-100 scale-75">
+      <a className="md:scale-100 scale-75 -ml-3 md:-ml-0" onClick={goHome}>
         <Logo />
-      </div>
+      </a>
 
-      <div className="md:scale-100 scale-90 mt-2 mr-2 text-primary">
+      <a
+        className="md:scale-100 scale-90 mt-2 mr-2 text-primary"
+        onClick={openMenu}
+      >
         <MenuIcon />
-      </div>
+      </a>
     </div>
   );
 };
