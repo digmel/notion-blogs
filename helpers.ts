4625fb888afc993ce3b716a961fb337163558cee
George <gio.digmel@gmail.com>
2023-03-25 00:33:00 -0500

    refactor: blog data management

--- helpers.ts
+++ helpers.ts
@@ -1,22 +1,29 @@
-export const generateBlogCover = (blog: any) => {
-  let cover = blog.cover;
+export const formatBlogsData = (publishedBlogs: any, allBlocks: any) => {
+  const data: any[] = [];
 
-  switch (cover?.type) {
-    case "file":
-      cover = blog.cover.file;
-      break;
-    case "external":
-      cover = blog.cover.external.url;
-      break;
-  }
+  publishedBlogs.forEach((blog: any, index: number) => {
+    let cover = blog.cover;
 
-  return {
-    id: blog.id,
-    cover: cover,
-    title: blog.properties.Name.title[0].plain_text,
-    tags: blog.properties.Tags.multi_select,
-    description: blog.properties.Description.rich_text[0].plain_text,
-    date: blog.properties.Updated.last_edited_time,
-    slug: blog.properties.Slug.formula.string,
-  };
+    switch (cover?.type) {
+      case "file":
+        cover = blog.cover.file;
+        break;
+      case "external":
+        cover = blog.cover.external.url;
+        break;
+    }
+
+    data.push({
+      id: blog.id,
+      cover: cover,
+      title: blog.properties.Name.title[0].plain_text,
+      tags: blog.properties.Tags.multi_select,
+      description: blog.properties.Description.rich_text[0].plain_text,
+      date: blog.properties.Updated.last_edited_time,
+      slug: blog.properties.Slug.formula.string,
+      blocks: allBlocks[index].results,
+    });
+  });
+
+  return data;
 };
