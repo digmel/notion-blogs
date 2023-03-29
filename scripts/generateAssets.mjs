bc1f3861cc6374aacab97ee7d7386d28a52cc2e9
George <gio.digmel@gmail.com>
2023-03-29 01:09:34 -0500

    fix: file paths

--- scripts/generateAssets.mjs
+++ scripts/generateAssets.mjs
@@ -1,6 +1,7 @@
 import fs from "fs";
 import fetch from "node-fetch";
 import sharp from "sharp";
+import path from "path";
 
 const imageUrl =
   "https://images.unsplash.com/photo-1516307318288-46d4194fe79e?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb";
@@ -38,19 +39,20 @@ const imageFormatter = async () => {
       effort: 6,
     });
 
-    // Output
-    if (!fs.existsSync("./out/assets")) {
-      fs.mkdirSync("./out/assets");
+    Output;
+    if (!fs.existsSync("/assets")) {
+      fs.mkdirSync("/assets");
       console.log("assets file is created");
     }
 
-    sharpImage.toFile(
-      `./out/assets/${OUTPUT_IMAGE_NAME}.webp`,
-      (error, info) => {
+    console.log("root", path.dirname("out"));
+
+    if (fs.existsSync("/assets")) {
+      sharpImage.toFile(`/assets/${OUTPUT_IMAGE_NAME}.webp`, (error, info) => {
         error && console.log("Error when writing file:", error);
         console.log("Output file details:", info);
-      }
-    );
+      });
+    }
   } catch ({ message }) {
     throw new Error(`Error from imageFormatter: ${message}`);
   }
