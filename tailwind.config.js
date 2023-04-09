4180bf2ce93e36a53676f7e6217fcf4422114a5d
George <gio.digmel@gmail.com>
2023-04-09 16:41:26 -0500

    chore: added design system colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#80888A",
-        light: "#ffff",
+        "gray-50": "#F9FAFB",
+        "gray-100": "#F3F4F6",
+        "gray-200": "#E5E7EB",
+        "gray-300": "#D1D5DB",
+        "gray-400": "#9CA3AF",
+        "gray-500": "#6B7280",
+        "gray-600": "#4B5563",
+        "gray-700": "#374151",
+        "gray-800": "#1F2937",
+        "gray-900": "#111827",
+        light: "#ffffff",
+        bright: "#FACF0E",
+        "sky-100": "#1192DC",
+        "sky-200": "#0B7CBC",
+        error: "#D52F45",
+        success: "#2FD55D",
       },
     },
     container: {
